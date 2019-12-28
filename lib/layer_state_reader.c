a84f655132205615d5af0ffcd483c60c51efe769
Nully <nully.nl@gmail.com>
2019-12-28 22:42:34 +0900

    :sparkles: add OLED libs

---
+++ lib/layer_state_reader.c
@@ -0,0 +1,30 @@
+
+#include QMK_KEYBOARD_H
+#include <stdio.h>
+#include "fancy65_ble.h"
+
+// in the future, should use (1U<<_LAYER_NAME) instead, but needs to be moved to keymap,c
+#define L_BASE 0
+#define L_LOWER 2
+#define L_RAISE 4
+
+char layer_state_str[24];
+
+const char *read_layer_state(void) {
+  switch (layer_state)
+  {
+  case L_BASE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    break;
+  case L_RAISE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+    break;
+  case L_LOWER:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Function");
+    break;
+  default:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Undef-%ld", layer_state);
+  }
+
+  return layer_state_str;
+}
